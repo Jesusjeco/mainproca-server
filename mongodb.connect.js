fa920ff536feca050ca7b0ba0307f3e62702de01
Jesusjeco <jesusenrique.carrero@gmail.com>
2024-07-12 11:48:58 -0400

    Modifyng product controller to endure Product model

--- mongodb.connect.js
+++ mongodb.connect.js
@@ -5,8 +5,8 @@ const env = process.env.NODE_ENV || 'development';
 dotenv.config({ path: `.env.${env}` });
 
 const mongoose = require('mongoose');
+
 const mongoURI = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@atlascluster.ru65j9u.mongodb.net/" + process.env.MONGO_DB_NAME + "?retryWrites=true&w=majority&appName=AtlasCluster";
-// const mongoURI = "mongodb+srv://jesusenriquecarrero:Jesusjeco19*@atlascluster.ru65j9u.mongodb.net/Mainproca?retryWrites=true&w=majority&appName=AtlasCluster";
 
 const connectDB = async () => {
   try {
